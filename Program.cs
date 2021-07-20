a52baa3bde5a833c5b0736a97875c608c1097ae6
Matheus O.G. do Nascimento <mogdns@gmail.com>
2021-07-20 16:10:43 -0300

    finalizar

--- Program.cs
+++ Program.cs
@@ -6,47 +6,77 @@ namespace logica
     class Program
     {
         static List<string> ListaNomes = new List<string>();
+        private static string pessoa;
 
+         static string escolha;
         static void Main(string[] args)
         {
 
-            string pessoa;
-
             do
             {
-                Console.WriteLine("Digite seu nome!");
-                string nome = Console.ReadLine();
+                Console.WriteLine($@" 
+                
+            | --------------------------------------------|
+            | -------------PAINEL DE CONTROLE ----------- |
+            | 1 - Cadastrar Nome                          |
+            | 2 - Listar Nome                             | 
+            |                                             |
+            | 3 - Finalizar Programa                      |
+            | --------------------------------------------| ");
+        
 
-                Console.WriteLine("Digite sua idade!");
-                int idade = int.Parse(Console.ReadLine());
+                escolha = (Console.ReadLine());
 
-                if (idade >= 16)
+                switch (escolha)
                 {
-                    Console.WriteLine("Entrada autorizada! Bom evento");
-                    ListaNomes.Add(nome);
+                    case "1":
+
+                        Console.WriteLine("Digite seu nome!");
+                        string nome = Console.ReadLine();
+
+                        Console.WriteLine("Digite sua idade!");
+                        int idade = int.Parse(Console.ReadLine());
+
+                        if (idade >= 16)
+                        {
+                            CadastraNome(nome);
+
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Você está acompanhado dos seus pais");
+                            string acompanhante = Console.ReadLine().ToLower();
+                            if (acompanhante == "s")
+                            {
+                                CadastraNome(nome);
+                            }
+                            else if (acompanhante == "n" && idade < 15)
+                            {
+                                Console.WriteLine($"Sua entrada não foi liberada!!!! Por favor volte ano que vem com seu resp onsavél legal. ");
+
+                            }
+                            else if (acompanhante == "n")
+                            {
+                                Console.WriteLine("Sua entrada não foi liberada, volte ano que vem.");
+                            }
+                        }
+
+                        break;
+                    case "2":
+                        MonstrarNome();
+                        break;
+                    case "3":
+                        Console.WriteLine("Você está saindo do nosso sistema.");
+                        break;
+                    default:
+                        Console.WriteLine($"Opção invalida por favor digite um dos números acima.");
+
+                        break;
                 }
-                else
-                {
-                    Console.WriteLine($"Você está acompanhado dos seus pais");
-                    string acompanhante = Console.ReadLine().ToLower();
-                    if (acompanhante == "s")
-                    {
-                        Console.WriteLine($"Sua entrada está autorizada!");
-                        ListaNomes.Add(nome);
-                    }
-                    else if (acompanhante == "n")
-                    {
-                        Console.WriteLine($"Sua entrada não foi liberada!!!! Por favor volte ano que vem. ");
 
-                    }
-                }
 
-                Console.WriteLine($"Deseja cadastrar mais uma pessoa? s/n");
-                pessoa = Console.ReadLine();
 
-            } while (pessoa == "s");
-
-            MonstrarNome();
+            } while (escolha != "3");
         }
 
         // lista
@@ -55,9 +85,15 @@ namespace logica
 
             foreach (var CadaItem in ListaNomes)
             {
-                Console.WriteLine($"nome {CadaItem}");
-
+                Console.WriteLine($"Nome: {CadaItem}");
             }
+        }
+
+        static void CadastraNome(string nome)
+        {
+
+            Console.WriteLine($"Sua entrada está autorizada!");
+            ListaNomes.Add(nome);
         }
     }
 }
